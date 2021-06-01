6d3cacbd9a6536e3ffc08861f9df8babf3fcd9a0
barcek <barcek@gmx.com>
2021-06-01 15:20:18 +0100

    docs: added docstring with interactive example to each function in 'phns/builder.py' & each method in 'phns/factory.py'

--- phns/builder.py
+++ phns/builder.py
@@ -12,6 +12,12 @@ iterables_passed = [list, tuple, set, frozenset, bytearray]
 # builder functions
 
 def get_functor(value: Any) -> Union[Functor, FunctorIter, FunctorDict]:
+    """Returns a base functor instance with a value property set to 'value'
+       of the class for either dictionary, other iterable or uniterable type.
+       >>> f = get_functor([1, 2, 3])
+       >>> print(f.__class__.__name__, f.value)
+       FunctorIter [1, 2, 3]
+    """
     const = get_constructor(value)
     if const in iterables_passed:
         return FunctorIter(value, const)
@@ -20,6 +26,12 @@ def get_functor(value: Any) -> Union[Functor, FunctorIter, FunctorDict]:
     return Functor(value)
 
 def get_pfunctor(value: Any) -> Union[PFunctor, PFunctorIter, PFunctorDict]:
+    """Returns a pointed functor instance with a value property set to 'value'
+       of the class for either dictionary, other iterable or uniterable type.
+       >>> pf = get_pfunctor([1, 2, 3])
+       >>> print(pf.__class__.__name__, pf.value)
+       PFunctorIter [1, 2, 3]
+    """
     const = get_constructor(value)
     if const in iterables_passed:
         return PFunctorIter.of(value, const)
--- phns/factory.py
+++ phns/factory.py
@@ -14,12 +14,29 @@ Builder = Callable[[P], Any]
 class Phnew():
 
     def __init__(self):
+        """Returns an instance of Phnew with an empty builders dictionary.
+           >>> p = Phnew()
+           >>> print(p.__class__.__name__, p.builders)
+           Phnew {}
+        """
         self.builders: Dict[str, Any] = {}
 
     def register(self, id: str, fn: Builder) -> None:
+        """Adds builder 'fn' to the builders dictionary keyed under 'id'.
+           >>> p = Phnew()
+           >>> p.register('get_int', lambda x: int(x))
+           >>> p.builders['get_int'](1)
+           1
+        """
         self.builders[id] = fn;
 
     def __call__(self, id: str, value: P) -> Any:
+        """Returns the result from the builder at 'id' when passed 'value'.
+           >>> p = Phnew()
+           >>> p.register('get_int', lambda x: int(x))
+           >>> p('get_int', 1)
+           1
+        """
         if id in self.builders:
             return self.builders[id](value)
         raise ValueError(id)
